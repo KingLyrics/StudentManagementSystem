83509dcbaaacc7669b9b19808ebe371c1c282ce7
aBugInTheSystem <ekom338.com@gmail.com>
2025-05-24 21:22:06 -0400

    Cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
