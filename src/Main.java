38214441b2435d793d762a15ab34e6952e71b662
aBugInTheSystem <ekom338.com@gmail.com>
2025-05-24 20:48:11 -0400

    Errors fixed

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Welcome to Student Management System");
+        ArrayList<Student> students = new ArrayList<>();
         while (true) {
-            System.out.println("Welcome to Student Management System");
+
             System.out.println("--------------------------------------");
             System.out.println("Option one: Add Student");
             System.out.println("Option two: View All Students");
@@ -15,10 +18,37 @@ public class Main {
 
             int option = sc.nextInt();
 
+
             if (option == 1) {
-                System.out.println("Adding Student...");
+                while(true) {
+                    System.out.println("Adding Student...");
+                    System.out.println("Enter Student ID: ");
+                    int studentID = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("Enter Student Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.println("Enter Student GPA: ");
+                    double gpa = sc.nextDouble();
+
+                    students.add(new Student(studentID, name, gpa));
+                    System.out.println("Student Added Successfully!");
+
+
+                    System.out.println("Continue? y/n");
+                    String cont = sc.next();
+                    if (Objects.equals(cont, "n") || Objects.equals(cont, "N")) {
+                        break;
+                    }
+                }
 
             }else if (option == 2) {
+                System.out.println("View All Students");
+
+                for (Student student : students){
+                    System.out.println(student);
+                }
 
             }else if (option == 3) {
 
--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,18 @@
 public class Student {
+    private final int studentID;
+    private final String name;
+    private double gpa;
+
+    Student(int studentID, String name, double gpa) {
+        this.studentID = studentID;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "studentID=" + studentID + ", name=" + name + ", gpa=" + gpa + '}';
+    }
+
 
 }
