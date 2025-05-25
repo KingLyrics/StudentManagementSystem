3e02c9ec78da00d2487be571ed17a350a823bc40
aBugInTheSystem <ekom338.com@gmail.com>
2025-05-24 21:22:26 -0400

    Reverted.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
